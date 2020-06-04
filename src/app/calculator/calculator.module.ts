5002c2c8c0c5454589af8d18642ef1304b636fb7
Williams Araujo <crfwillams@gmail.com>
2020-06-04 17:35:09 +0100

    Inject service dependence in component

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './components';
+import { CalculatorService } from './services';
 
 @NgModule({
   declarations: [CalculatorComponent],
   imports: [
     CommonModule
   ],
-  exports: [CalculatorComponent]
+  exports: [CalculatorComponent],
+  providers: [CalculatorService]
 })
 export class CalculatorModule { }
--- src/app/calculator/components/calculator.component.ts
+++ src/app/calculator/components/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from '../services';
 
 @Component({
   selector: 'app-calculator',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void {
   }
